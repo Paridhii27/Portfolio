4394547e5a9f3f2d6ca6e23941bdabb5b2289e50
Paridhi <paridhigarg27@gmail.com>
2023-07-21 21:32:08 -0400

    navigation menu fix

---
+++ resume.js
@@ -0,0 +1,14 @@
+function downloadImage() {
+    var downloadLink = document.getElementById("downloadLink");
+    downloadLink.click();
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
